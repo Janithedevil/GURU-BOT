f14bbc52693365bd90f5b71b285e73dec252bbe0
Janithedevil <154800445+Janithedevil@users.noreply.github.com>
2023-12-26 17:41:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923316034576', 'Zaman Baloch', true],
+  ['923337981452', 'janiii', true], 
   [''] 
 ] //Number of owners
 
